9fa08f94f280a653b5f3c6cf6f36e50d873c75a7
zhyie <128297362+zhyie@users.noreply.github.com>
2026-05-29 06:16:37 +0800

    Activate patches

---
+++ patches.h
@@ -0,0 +1,67 @@
+/**** ACTIVE PATCHES ****/
+
+#define BORDER_PATCH 1
+#define CASEINSENSITIVE_PATCH 1
+#define CENTER_PATCH 1
+#define CTRL_V_TO_PASTE_PATCH 1
+#define DYNAMIC_HEIGHT_PATCH 1
+#define FUZZYMATCH_PATCH 1
+#define FZFEXPECT_PATCH 1
+/* Known issue: highlighting does not work properly when pango markup is used */
+#define HIGHLIGHT_PATCH 1
+#define HIGHPRIORITY_PATCH 1
+#define MOUSE_SUPPORT_PATCH 1
+#define MOTION_SUPPORT_PATCH 1
+/* Uncomment the line in the dmenu_run script which replaces the exec command. */
+#define NAVHISTORY_PATCH 1
+#define NUMBERS_PATCH 1
+#define QUIET_PATCH 1
+#define REJECTNOMATCH_PATCH 1
+/* Known issue: not compatible with the pango patch */
+#define SCROLL_PATCH 1
+#define VERTFULL_PATCH 1
+#define VI_MODE_PATCH 1
+#define VI_MODE_RUNTIME_STARTING_MODE_PATCH 1
+#define WMTYPE_PATCH 1
+
+
+
+/**** INACTIVE PATCHES ****/
+
+#define ALPHA_PATCH 0
+#define BARPADDING_PATCH 0
+#define BIDI_PATCH 0
+#define CARET_SCHEME_PATCH 0
+#define CARET_WIDTH_PATCH 0
+#define DYNAMIC_OPTIONS_PATCH 0
+#define EMOJI_HIGHLIGHT_PATCH 0
+#define GRID_PATCH 0
+#define GRIDNAV_PATCH 0
+#define INCREMENTAL_PATCH 0
+#define INITIALTEXT_PATCH 0
+#define INPUTMETHOD_PATCH 0
+#define INSTANT_PATCH 0
+#define LINE_HEIGHT_PATCH 0
+#define MANAGED_PATCH 0
+#define MORECOLOR_PATCH 0
+#define MULTI_SELECTION_PATCH 0
+#define NO_COLOR_EMOJI_PATCH 0
+#define NO_SORT_PATCH 0
+#define NON_BLOCKING_STDIN_PATCH 0
+/* Uncomment the corresponding lines in config.mk to use the pango libraries
+ * Known issue: not compatible with the scroll patch */
+#define PANGO_PATCH 0
+#define PASSWORD_PATCH 0
+#define PIPEOUT_PATCH 0
+#define PLAIN_PROMPT_PATCH 0
+#define PREFIXCOMPLETION_PATCH 0
+#define PRESELECT_PATCH 0
+#define PRINTINDEX_PATCH 0
+#define PRINTIPUTTEXT_PATCH 0
+#define RELATIVE_INPUT_WIDTH_PATCH 0
+#define RESTRICT_RETURN_PATCH 0
+#define SEPARATOR_PATCH 0
+#define SYMBOLS_PATCH 0
+#define TSV_PATCH 0
+#define XRESOURCES_PATCH 0
+#define XYW_PATCH 0
